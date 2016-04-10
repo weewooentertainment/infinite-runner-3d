e69e358f7e6207eeefdc31505127011e4aff2126
Dennis Yau <denn.yau@gmail.com>
2016-04-10 16:44:02 -0400

    dennis kinda works, since i use Z instead of X the travel math is fucking up

---
+++ Assets/Galaxy_Roll_Colab/SparseController.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SparseController : MonoBehaviour {
+
+	public GameObject Road;
+	public GameObject Coin;
+	public List<Material> materials = new List<Material>();
+
+	private List<Transform> Cpoints = new List<Transform>();
+	private List<GameObject> CurrentRoads = new List<GameObject>();
+	private List<GameObject> CurrentCoins = new List<GameObject>();
+	private int CurrentPoint = 0;
+	private int positionNum = 0;
+	private int CurrentRoadId = 0;
+	private int LevelCount = 0;
+	private float time = 0;
+	private float distance = 0, nextDistance = 0;
+	private float speed = 1.5f;
+	private Transform CenterPoints;
+	//public GameObject characterSphere;
+	private int prefab_P = 0;
+
+	void Start () {
+		prefab_P = GameObject.Find ("Points").transform.childCount;
+		//print ("prefab_P: " + prefab_P);
+		//characterSphere = 
+		//Transform beginPoint = GameObject.Find("CharacterSphere").transform;
+		//Cpoints.Add (beginPoint);
+		CreateNewRoad();
+
+	}
+
+	void CreateNewRoad()
+	{
+		for (int i = CurrentCoins.Count - 1; i >= 0; i--) {
+			if(CurrentCoins[i].transform.position.z < this.transform.position.z)
+			{
+				DestroyImmediate(CurrentCoins[i]);
+				CurrentCoins.RemoveAt(i);
+			}
+		}
+		if(CurrentRoadId > 0)
+		{
+			if(CurrentRoadId > 1)
+			{
+				DestroyImmediate(CurrentRoads[CurrentRoadId - 2]);
+				CurrentRoads.RemoveAt(0);
+				CurrentRoadId--;
+			}
+			
+			CurrentRoads.Add(Instantiate (Road, Cpoints[LevelCount * (prefab_P - 1) + LevelCount - 1].position, 
+			                                Quaternion.identity) as GameObject);
+		}
+		else
+			CurrentRoads.Add(Instantiate (Road, Vector3.zero, Quaternion.identity) as GameObject);
+
+		CenterPoints = CurrentRoads[CurrentRoadId].transform.FindChild("Plane").FindChild("Points");
+		for (int i = 1; i < prefab_P; i++) 
+		{
+			Transform childPoint = CenterPoints.transform.FindChild("P" + i);
+			Cpoints.Add(childPoint);
+
+			int typeNum = Random.Range(0,  materials.Count);
+			Vector3 cPOS = Vector3.zero;
+			
+			if(LevelCount > 0)
+				cPOS = new Vector3(childPoint.transform.position.x + Random.Range(-1, 2) * 1.0f,
+				                   childPoint.transform.position.y - 0.5f, 
+				                   childPoint.transform.position.z);
+			else
+				cPOS = new Vector3(Cpoints[i - 1].position.x + Random.Range(-1,2) * 1.0f,Cpoints[i - 1].position.y - 0.5f, Cpoints[i - 1].position.z);
+			/*
+			GameObject newCoin = (Instantiate(Coin,cPOS,Quaternion.identity) as GameObject);
+			CurrentCoins.Add(newCoin);
+			newCoin.transform.Rotate(90,0,90);
+			newCoin.transform.GetComponent<Renderer>().material = materials[typeNum];
+			
+			if(typeNum == 0)
+				newCoin.name = "Yellow";
+			
+			else if(typeNum == 1)
+				newCoin.name = "Red";
+			
+			else if(typeNum == 2)
+				newCoin.name = "Blue";
+			
+			else if(typeNum == 3)
+				newCoin.name = "Black";
+				*/
+		}
+
+
+	}
+
+	
+	void Update () {
+		/* ORIGINAL ASSUMES WE TRAVEL ON X
+		Vector3 startPoint = new Vector3 (Cpoints [CurrentPoint].position.x + distance, 
+		                                  Cpoints [CurrentPoint].position.y - 0.5f, 
+		                                  Cpoints [CurrentPoint].position.z);
+		Vector3 endPoint = new Vector3 (Cpoints [CurrentPoint + 1].position.x + distance, 
+		                                Cpoints [CurrentPoint + 1].position.y - 0.5f, 
+		                                Cpoints [CurrentPoint + 1].position.z);
+		*/
+		// We used Z
+		Vector3 startPoint = new Vector3 (Cpoints [CurrentPoint].position.x, 
+			Cpoints [CurrentPoint].position.y - 0.5f, 
+			Cpoints [CurrentPoint].position.z + distance);
+		Vector3 endPoint = new Vector3 (Cpoints [CurrentPoint + 1].position.x, 
+			Cpoints [CurrentPoint + 1].position.y - 0.5f, 
+			Cpoints [CurrentPoint + 1].position.z + distance);
+		
+		transform.position = Vector3.Lerp (startPoint, endPoint, time);
+		transform.LookAt(Vector3.Lerp(Cpoints[CurrentPoint + 1].transform.position, 
+		                                     Cpoints[CurrentPoint + 2].transform.position,time));
+		time += Time.deltaTime * speed;
+
+		if(time > 1)
+		{
+			time = 0;
+			//CurrentPoint++;
+			CurrentPoint = 0;
+			string generatorName = "P" + (prefab_P - 3);
+			if(Cpoints [CurrentPoint].name == generatorName)
+			{
+				CurrentRoadId++;
+				LevelCount++;
+				CreateNewRoad();
+			}
+
+		}
+		
+		
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			if(positionNum != -1)
+			{
+				positionNum--;
+				nextDistance = 3.0f * positionNum;
+			}
+		}
+		
+		else if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			if(positionNum != 1)
+			{
+				positionNum++;
+				nextDistance = 3.0f  * positionNum;
+			}
+		}
+		
+		distance = Mathf.Lerp (distance, nextDistance, Time.deltaTime * 3 * speed);
+	
+	}
+}
